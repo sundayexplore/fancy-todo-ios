f88511b3907458a7b99bd0b04a1c0b6e801f85c9
Adam Rafiandri <rafiandria23@gmail.com>
2021-07-08 21:44:42 +0700

    Initial Commit

---
+++ FancyTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FancyTodo
+//
+//  Created by Adam Rafiandri on 7/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FancyTodo/FancyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancyTodoApp.swift
+//  FancyTodo
+//
+//  Created by Adam Rafiandri on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct FancyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
