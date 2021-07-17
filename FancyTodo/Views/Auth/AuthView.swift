2a45b781484ac9ac02c338aba0ae82e279054373
Adam Rafiandri <rafiandria23@gmail.com>
2021-07-17 18:09:01 +0700

    Created AuthView.

---
+++ FancyTodo/Views/Auth/AuthView.swift
@@ -0,0 +1,41 @@
+//
+//  AuthView.swift
+//  FancyTodo
+//
+//  Created by Adam Rafiandri on 7/11/21.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .center, spacing: 200) {
+                Text("Fancy Todo")
+                    .bold()
+                    .font(.largeTitle)
+                
+                VStack {
+                    NavigationLink(destination: SignInView()) {
+                        HStack {
+                            Image(systemName: "envelope")
+                                .font(.callout)
+                            Text("Continue with email")
+                                .fontWeight(.semibold)
+                                .font(.callout)
+                        }
+                        .foregroundColor(Color.black)
+                        .padding()
+                        .border(Color.black, width: 2)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView()
+    }
+}
